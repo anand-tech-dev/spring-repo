42f706b928e25dec4da98c1506ab02ff0da2c81f
anand2git <107996634+anand2git@users.noreply.github.com>
2022-09-01 00:56:24 +0530

    Updated the comments at class level

--- src/main/java/com/howtodoinjava/rest/SpringBootDemoApplication.java
+++ src/main/java/com/howtodoinjava/rest/SpringBootDemoApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+/**
+ *  Bootstrap and launch a Spring application from a Java main method. 
+ * @author rajas
+ *
+ */
+
 @SpringBootApplication
 @EnableSwagger2
 public class SpringBootDemoApplication {
